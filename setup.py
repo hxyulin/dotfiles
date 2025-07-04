c5c2602e844618fe51ee6193e025d22f6725820d
hxyulin <hxyulin@proton.me>
2025-07-04 14:44:14 +0800

    [REFACTOR]: Refactored setup script to be easier to use and more resilient to errors

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@
 
 import sys
 import os
+import platform
+from dataclasses import dataclass
+
+PLATFORM = platform.system()
 
 def get_config_dir():
     # If we are on windows, it would be located at the appdata directory
-    if sys.platform == 'win32':
+    if PLATFORM == "Windows":
         return os.environ['APPDATA']
     else:
         return os.path.expanduser('~/.config')
@@ -13,101 +17,148 @@ def get_config_dir():
 CONFIG_DIR = get_config_dir()
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 # TODO: Read env variable and or arguments
-CONFIG_DIR = "~/.config"
 
-CONFIGURATIONS = {
-    "nvim": {
-        "platforms": [],
-        "src": "nvim",
-        "dest": f"{CONFIG_DIR}/nvim",
-        "deps": [],
+@dataclass
+class Configuration:
+    platforms: list[str]
+    src: str
+    dest: str
+    deps: list[str]
+    setup: None
+
+    def is_available(self) -> bool:
+        return len(self.platforms) == 0 or (PLATFORM in self.platforms)
+
+CONFIGURATIONS: dict[str, Configuration] = {
+    "nvim": Configuration(
+        platforms=[],
+        src="nvim",
+        dest=f"{CONFIG_DIR}/nvim",
+        deps=[],
         # Additional setup function
-        "setup": None,
-    },
-    "starship": {
-        "platforms": [],
-        "src": "starship.toml",
-        "dest": f"{CONFIG_DIR}/starship.toml",
-        "deps": [],
-        # "TODO": Find shell, and give instructions for installation
-        "setup": None,
-    },
-    "nix-darwin": {
-        "platforms": ["darwin"],
-        "src": "nix-darwin",
-        "dest": f"{CONFIG_DIR}/nix-darwin",
-        "deps": [],
-        "setup": None,
-    }
+        setup=None,
+    ),
+    "starship": Configuration(
+        platforms=[],
+        src="starship.toml",
+        dest=f"{CONFIG_DIR}/starship.toml",
+        deps=[],
+        # TODO=Find shell, and give instructions for installation
+        setup=None,
+    ),
+    "nix-darwin": Configuration(
+        platforms=["Darwin"],
+        src="nix-darwin",
+        dest=f"{CONFIG_DIR}/nix-darwin",
+        deps=[],
+        setup=None,
+    ),
+    "ghostty": Configuration(
+        platforms=[],
+        src="ghostty",
+        dest=f"{CONFIG_DIR}/ghostty",
+        deps=[],
+        setup=None,
+    ),
 }
 
-def symlink(source, target):
+def symlink(source: str, target: str):
     if os.path.exists(target):
-        print(f"Target {target} already exists.")
+        print(f"installation destination {target} already exists, please review and remove it manually!")
         sys.exit(1)
     os.symlink(source, target)
 
-def unlink(target):
-    if os.path.exists(target):
+def unlink(target: str):
+    if not os.path.islink(target):
+        print(f"{target} is not symlinked, please remove it manually if you are sure!")
+        sys.exit(1)
+    if os.path.exists(target) and os.path.islink(target):
         os.unlink(target)
+    else:
+        print(f"cannot unlink {target}, because it doesn't exist or isn't a symlink")
 
-def print_help():
-    name = sys.argv[0]
-    print(f'Usage: {name} install <configuration | all>')
-    print(f'Usage: {name} uninstall <configuration | all>')
-    print(f'Usage: {name} list')
-    sys.exit(1)
+def install(config: Configuration):
+    source = os.path.join(SCRIPT_DIR, config.src)
+    print(f"symlinking {config.dest} -> {source}")
+    symlink(source, config.dest)
 
-def install(configurations):
-    config_dir = get_config_dir()
-    script_dir = get_script_dir()
-    print(f"Installing configurations to {config_dir}")
-
-    for configuration in configurations:
-        source = os.path.join(script_dir, configuration)
-        target = os.path.join(config_dir, configuration)
-        symlink(source, target)
-
-def uninstall(configurations):
-    config_dir = get_config_dir()
-    print(f"Uninstalling configurations from {config_dir}")
-    for configuration in configurations:
-        target = os.path.join(config_dir, configuration)
-        unlink(target)
-
+def uninstall(config: Configuration):
+    unlink(config.dest)
 
 def main():
     if len(sys.argv) < 2:
-        print_help()
+        name = sys.argv[0]
+        print(f'Usage: {name} install [configuration]')
+        print(f'Usage: {name} uninstall [configuration]')
+        print(f"Usage: {name} info <configuration>")
+        print(f'Usage: {name} list')
+        sys.exit(1)
     command = sys.argv[1]
 
-    if command == 'list':
-        print(CONFIGURATIONS)
-        sys.exit(0)
-
-    if command == 'install':
-        if len(sys.argv) < 3:
-            print_help()
-        configuration = sys.argv[2]
-        if configuration == 'all':
-            install(configurations)
+    match command:
+        case 'list':
+            unavailable: int = 0
+            for name,conf in CONFIGURATIONS.items():
+                if not conf.is_available():
+                    unavailable += 1;
+                    continue
+                print(name)
+            print(f"... and {unavailable} more not supported for the current platform")
             sys.exit(0)
-        if configuration not in configurations:
-            print(f'Configuration {configuration} not found.')
-            sys.exit(1)
-        install([configuration])
-
-    elif command == 'uninstall':
-        if len(sys.argv) < 3:
-            print_help()
-        configuration = sys.argv[2]
-        if configuration == 'all':
-            uninstall(configurations)
+        case 'info':
+            if len(sys.argv) < 3:
+                print("requires configuration name")
+                sys.exit(1)
+            name = sys.argv[2]
+            try:
+                conf = CONFIGURATIONS[name]
+                print(f"Configuration '{name}'")
+                if len(conf.platforms) != 0:
+                    print(f"Available for platforms: {",".join(conf.platforms)}")
+                else:
+                    print(f"Available for all platforms")
+                print(f"Configuration destination: '{conf.dest}'")
+            except KeyError: 
+                print("invalid configuration")
+                sys.exit(1)
             sys.exit(0)
-        if configuration not in configurations:
-            print(f'Configuration {configuration} not found.')
-            sys.exit(1)
-        uninstall([configuration])
+        case 'install':
+            if len(sys.argv) < 3:
+                print("installing all configurations...")
+                for name,conf in CONFIGURATIONS.items():
+                    if not conf.is_available():
+                        continue
+                    print(f"installing {name}...")
+                    install(conf)
+            else:
+                name = sys.argv[2]
+                try:
+                    install(CONFIGURATIONS[name])
+                except KeyError:
+                    print("invalid configuration")
+                    sys.exit(1)
+                sys.exit(0)
+        case 'uninstall' | 'remove':
+            if len(sys.argv) < 3:
+                if input("are you sure you want to remove all configurations? Y/n").lower() == 'n':
+                    print("cancelled")
+                    sys.exit(1)
+                for name,conf in CONFIGURATIONS.items():
+                    if not conf.is_available():
+                        continue
+                    print(f"removing {name}...")
+                    uninstall(conf)
+            else:
+                name = sys.argv[2]
+                try:
+                    uninstall(CONFIGURATIONS[name])
+                except KeyError:
+                    print("invalid configuration")
+                    sys.exit(1)
+                sys.exit(0)
+
+        case _:
+            print("invalid subcommand")
 
 if __name__ == '__main__':
     main()
